226975c185b00da19c63b7e408f4aef10618e53a
furkan <furkan.atak@stu.fsm.edu.tr>
2022-04-09 16:59:47 +0300

    #appbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="#F5F7FAFA"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          @click="goTo('/')"
-          cursor="click"
-          alt="Vuetify Logo"
-          class="shrink mr-1"
-          contain
-          src="./assets/logo.png"
-          transition="scale-transition"
-          width="50"
-        />
-        <v-img
-          @click="goTo('/')"
-          alt="Vuetify Logo"
-          class="shrink mr-1"
-          contain
-          src="./assets/ekmek.png"
-          transition="scale-transition"
-          width="150"
-        />
-      </div>
-       
-      <v-spacer></v-spacer>
-     
-      <v-btn
-        href="https://github.com/furkan-atak/ekmek-cli"
-        target="_blank"
-        text
-      >
-        <span class="mr-2" style="color: orange">Hakkımızda</span>
-        <v-icon style="color: orange;">mdi-open-in-new</v-icon>
-      </v-btn>
-       <vs-button @click="goTo('/login')" flat >Login</vs-button>
-      <vs-button>Get Started</vs-button>
-      
-    </v-app-bar>
-
+    
+    <core-app-bar></core-app-bar>
     <v-main>
       <router-view/>
     </v-main>
@@ -56,6 +16,9 @@ export default Vue.extend({
   data: () => ({
     //
   }),
+  components: {
+    CoreAppBar: () => import("@/components/core/AppBar.vue")
+  },  
   methods: {
     goTo(path: string, query?: any) {
     const url = path.startsWith("/", 0) ? path : "/".concat(path);
---
+++ src/components/core/AppBar.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-app-bar
+        app
+        color="#F5F7FAFA"
+        dark
+        >
+        <div class="d-flex align-center">
+            <v-img
+            @click="goTo('/')"
+            style="cursor: pointer;"
+            alt="Vuetify Logo"
+            class="shrink mr-1"
+            contain
+            src="../../assets/logo.png"
+            transition="scale-transition"
+            width="50"
+            />
+            <v-img
+            @click="goTo('/')"
+            style="cursor: pointer;"
+            alt="Vuetify Logo"
+            class="shrink mr-1"
+            contain
+            src="../../assets/ekmek.png"
+            transition="scale-transition"
+            width="150"
+            />
+        </div>
+        
+        <v-spacer></v-spacer>
+        
+        <v-btn
+            href="https://github.com/furkan-atak/ekmek-cli"
+            target="_blank"
+            text
+        >
+            <span class="mr-2" style="color: orange">Hakkımızda</span>
+            <v-icon style="color: orange;">mdi-open-in-new</v-icon>
+        </v-btn>
+        <vs-button @click="goTo('/login')" flat >Login</vs-button>
+        <vs-button>Get Started</vs-button>
+    </v-app-bar>
+</template>    
+
+<script lang="ts">
+    import { Component } from 'vue-property-decorator';
+    import BaseView from '../../views/baseView';
+    @Component
+    export default class AppBar extends BaseView{
+    constructor() {
+        super();
+    }
+    
+
+    /* eslint-disable */
+    goTo(path: string, query?: any) {
+        this.navigate(path, query);
+    }
+}
+</script>
+
+
