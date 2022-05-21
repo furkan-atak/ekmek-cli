926da8eab5ede7092a795a191a466e3e90e0836c
furkan <furkan.atak@stu.fsm.edu.tr>
2022-05-21 22:32:09 +0300

    homepage css

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,80 +1,72 @@
 <template>
   <div style="margin-top: 0.8cm;">
     <v-carousel>   
-      <v-carousel-item
-        v-for="(item,i) in itemk"
-        :key="i"
-        :src="item.src"
-        reverse-transition="fade-transition"
-        transition="fade-transition"
-      ></v-carousel-item>
-      
-  </v-carousel>
+        <v-carousel-item
+          v-for="(item,i) in itemk"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>    
+    </v-carousel>
   <br>
   <br>
-  <v-row>
-        
-        <v-col>
-          <v-row >   
-          <h1 style="padding-left: 5%;"> En Popüler Kategoriler </h1>         
-          <v-container style="margin-left: 8%" class="pa-12 text-center">
-            <v-row
-              class="fill-height justify-center"
-            >
-              <template v-for="(item, i) in items">
-                <v-col
-                  style="margin-left: 3.3%;"
-                  :key="i"
-                  cols="12"
-                  md="2"
+  <v-row>          
+    <h1 style="padding-left: 5%;"> En Popüler Kategoriler </h1>  
+  </v-row>       
+    <v-container style="margin-right: 18%; width: 100%;">
+      <v-row style="width: 1300px;">
+        <template v-for="(item, i) in items">
+          <v-col
+            :style="i === 0 ? 'margin-left: 5%;' :  'margin-left: 11%;'"
+            :key="i"
+            cols="12"
+            md="1"
+          >
+            <v-hover v-slot="{ hover }">
+              <v-card
+                @click="goTo(item.path)"
+                style="width: 210px; max-width: 400px; cursor: pointer; margin-left: 4%; margin: 5%;"
+                :elevation="hover ? 24 : 2"
+                :class="{ 'on-hover': hover }"
+              >
+                <v-img
+                  :src="item.img"
+                  height="350px"
                 >
-                  <v-hover v-slot="{ hover }">
-                    <v-card
-                      @click="goTo(item.path)"
-                      style="width: 210px; max-width: 400px; cursor: pointer; margin-left: 4%; margin: 5%;"
+                  <v-card-title class="text-h6 white--text">
+                    <v-row
                       :elevation="hover ? 24 : 2"
-                      :class="{ 'on-hover': hover }"
+                      class="fill-height flex-column"
+                      justify="space-between"
                     >
-                      <v-img
-                        :src="item.img"
-                        height="350px"
-                      >
-                        <v-card-title class="text-h6 white--text">
-                          <v-row
-                            :elevation="hover ? 24 : 2"
-                            class="fill-height flex-column"
-                            justify="space-between"
-                          >
-                            <p :style="hover ? 'text-decoration: underline;' : 'text-decoration:none;'" class="mt-4 subheading text-left">
-                              {{ item.title }}
-                            </p>
-                            <div>
-                              <p class="ma-0 text-body-1 font-weight-bold font-italic text-left">
-                                {{ item.text }}
-                              </p>
-                            </div>
-                            <div class="align-self-center">
-                              <v-btn
-                                v-for="(icon, index) in icons"
-                                :key="index"
-                                :class="{ 'show-btns': hover }"
-                                icon
-                              >
-                              </v-btn>
-                            </div>
-                          </v-row>
-                        </v-card-title>
-                      </v-img>
-                    </v-card>
-                  </v-hover>
-                </v-col>
-              </template>
-            </v-row>
-          </v-container>
-        </v-row>
-        </v-col>      
-        </v-row>
-        <div style="margin: 2%;"></div>
+                      <p :style="hover ? 'text-decoration: underline;' : 'text-decoration:none;'" class="mt-4 subheading text-left">
+                        {{ item.title }}
+                      </p>
+                      <div>
+                        <p class="ma-0 text-body-1 font-weight-bold font-italic text-left">
+                          {{ item.text }}
+                        </p>
+                      </div>
+                      <div class="align-self-center">
+                        <v-btn
+                          v-for="(icon, index) in icons"
+                          :key="index"
+                          :class="{ 'show-btns': hover }"
+                          icon
+                        >
+                        </v-btn>
+                      </div>
+                    </v-row>
+                  </v-card-title>
+                </v-img>
+              </v-card>
+            </v-hover>
+          </v-col>
+        </template>
+      </v-row>
+    </v-container>    
+   <div style="margin: 2%;"></div>
   </div>
 </template>
 
